f30988baa20307ff94ee09285d14ac57d6f411e0
S Fasna <83363902+FasnaSharaf@users.noreply.github.com>
2023-05-03 16:18:22 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //code to connect express application to mongodb
 const mongoose = require('mongoose');
-const uri = "mongodb://root:example@RESTapi:27018/";
+const uri = "mongodb://root:example@notes-app-api-RESTapi-1:27018/";
 const connectDB = async () => {
     try {
         await mongoose.connect(uri , 
