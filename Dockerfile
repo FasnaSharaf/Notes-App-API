971e934295215a73c256d617829e501693dc3708
Ubuntu <ubuntu@ip-172-31-81-3.ec2.internal>
2023-05-10 08:38:40 +0000

    added monitoring stack and ci cd conatiners

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:16-alpine
 WORKDIR /app
 COPY . /app
 RUN npm install
-EXPOSE 5000
+EXPOSE 3000
 HEALTHCHECK --interval=30s --timeout=10s \
   CMD wget -q http://localhost:5000/health -O /dev/null || exit 1
 CMD ["npm", "start"]
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 //code to connect express application to mongodb
 const mongoose = require('mongoose');
-const uri = "mongodb://root:example@172.19.0.3:27018/mydatabase";
+const uri = "mongodb://root:example@172.23.0.2:27017/mydatabase";
 const connectDB = async () => {
     try {
         await mongoose.connect(uri , 
